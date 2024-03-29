29477f0fbe28edcc7d6dd270815d1838f47b3320
maxcacacho <jean.cacacho@student.ateneo.edu>
2024-03-30 01:14:34 +0800

    keymap for netrw

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
+-- editor basics
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+-- fundamental keymaps
+vim.g.mapleader = ' '
+
+vim.api.nvim_set_keymap('n', '<Leader>pv', ':Explore<CR>', { noremap = true, silent = true })
